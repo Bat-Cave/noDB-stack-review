351030732788938efc19a35de02a81a13385d5fc
Rico Hancock <richardl@hancock1.com>
2020-01-23 10:01:17 -0700

    morning review

---
+++ db/add_employee.sql
@@ -0,0 +1,6 @@
+INSERT INTO employees
+(first, last, email, gender)
+values
+($1, $2, $3, $4);
+
+select * from employees;
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -3,37 +3,80 @@ let id = 21
 
 module.exports = {
     getEmployees: (req, res) => {
-        res.status(200).send(employees)
+        const db = req.app.get('db');
+
+        db.get_employees()
+        .then(response => res.status(200).send(response))
+        .catch(err => {
+            res.status(500).send({errMessage: 'Something broke'})
+            console.log(err)
+
+        })
+        
+        // res.status(200).send(employees)
     },
 
     editEmployee: (req, res) => {
-        const {id} = req.params
-        const index = employees.findIndex(person => person.id === +id)
-        employees.splice(index, 1, req.body)
-        console.log(employees)
-        res.status(200).send(employees)
+        const db = req.app.get('db');
+        const {id} = req.params;
+        const {first, last, email, gender} = req.body;
+
+        db.edit_employee([id, first, last, email, gender])
+        .then(response => res.status(200).send(response))
+        .catch( err => {
+            res.status(500).send({errMessage: 'Something broke'});
+            console.log(err)
+        })
+
+
+        // const {id} = req.params
+        // const index = employees.findIndex(person => person.id === +id)
+        // employees.splice(index, 1, req.body)
+        // console.log(employees)
+        // res.status(200).send(employees)
     },
 
     addEmployee: (req, res) => {
-        console.log('req.body', req.body)
-        const {firstName, lastName, email, gender} = req.body
-        const newEmployee = {
-            id,
-            firstName,
-            lastName,
-            email,
-            gender
-        }
-        id++
-        employees.push(newEmployee)
-        res.status(200).send(employees)
+        const db = req.app.get('db');
+        const {first, last, email, gender} = req.body;
+
+
+        db.add_employee([first, last, email, gender])
+        .then(response => res.status(200).send(response))
+        .catch(err => {
+            res.status(500).send({errMessage: 'Something broke'});
+            console.log(err)
+        })
+
+        // console.log('req.body', req.body)
+        // const {firstName, lastName, email, gender} = req.body
+        // const newEmployee = {
+        //     id,
+        //     firstName,
+        //     lastName,
+        //     email,
+        //     gender
+        // }
+        // id++
+        // employees.push(newEmployee)
+        // res.status(200).send(employees)
     },
 
     deleteEmployee: (req, res) => {
-        const {id} = req.params
-        const index = employees.findIndex(person => person.id === +id)
-        employees.splice(index, 1)
-        console.log(employees)
-        res.status(200).send(employees)
+        const db = req.app.get('db');
+        const {id} = req.params;
+
+        db.delete_employee(id)
+        .then(response => res.status(200).send(response))
+        .catch(err => {
+            res.status(500).send({errMessage: 'Something broke'})
+            console.log(err);
+        })
+
+        // const {id} = req.params
+        // const index = employees.findIndex(person => person.id === +id)
+        // employees.splice(index, 1)
+        // console.log(employees)
+        // res.status(200).send(employees)
     }
 }
